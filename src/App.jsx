7a18f1d91648f062d5f9164d7c56d84f35e0d483
VictorAndres198 <andresroh98@hotmail.com>
2023-10-02 20:50:28 -0500

    Agregando pagina al repo

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Navbar from './pages/components/NavBar'
+
+function App() {
+  return (
+    <div>
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+      </Routes>
+    </div>
+  )
+}
+
+export default App
